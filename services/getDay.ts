262e2c4997c1634f88be69359adc89c768f0b9a1
Cody Clark <opuseleven@protonmail.com>
2022-01-15 17:19:43 -0600

    Added /services/getDay

---
+++ services/getDay.ts
@@ -0,0 +1,14 @@
+import { Day } from '../types';
+
+function getDay(dayData: any) {
+  const day: Day = {
+    weather: dayData['weather'][0]['main'],
+    temp: parseInt(String((dayData['main']['temp'] * 1.8 - 459.67)), 10),
+    humidity: dayData['main']['humidity'],
+    weatherDescription: dayData['weather'][0]['description'],
+    precipitation: Number(dayData['pop']) * 100,
+    weatherId: parseInt(dayData['weather'][0]['id'])
+  }
+  return day;
+}
+export { getDay };
