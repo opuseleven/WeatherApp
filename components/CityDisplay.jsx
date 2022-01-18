41db05be747f55fc5d610dc7911a5ddfcd6ffc50
Cody Clark <opuseleven@protonmail.com>
2022-01-18 01:00:48 -0600

    Added useEffect

--- components/CityDisplay.jsx
+++ components/CityDisplay.jsx
@@ -1,29 +1,36 @@
+import { useState, useEffect } from 'react';
+
 function CityDisplay({ city }) {
 
-  const cityName = city.name;
+  const [ state, setState ] = useState(city);
 
-  let lat = Number(city.lat);
-  let lon = Number(city.lon);
-  let latDir = 'N';
-  let lonDir = 'E';
-  if (lat < 0) {
-    latDir = 'S';
-    lat = lat * -1;
-  }
-  if (lon < 0) {
-    lonDir = 'W';
-    lon = lon * -1;
-  }
+  useEffect(() => {
+    const cityName = city.name;
 
-  const degreesLat = Math.floor(lat);
-  const degToMinLat = (lat - degreesLat) * 60;
-  const minutesLat = Math.floor(degToMinLat);
-  const secondsLat = Math.floor((degToMinLat - minutesLat) * 60);
+    let lat = Number(city.lat);
+    let lon = Number(city.lon);
+    let latDir = 'N';
+    let lonDir = 'E';
+    if (lat < 0) {
+      latDir = 'S';
+      lat = lat * -1;
+    }
+    if (lon < 0) {
+      lonDir = 'W';
+      lon = lon * -1;
+    }
 
-  const degreesLon = Math.floor(lon);
-  const degToMinLon = (lon - degreesLon) * 60;
-  const minutesLon = Math.floor(degToMinLon);
-  const secondsLon = Math.floor((degToMinLon - minutesLon) * 60);
+    const degreesLat = Math.floor(lat);
+    const degToMinLat = (lat - degreesLat) * 60;
+    const minutesLat = Math.floor(degToMinLat);
+    const secondsLat = Math.floor((degToMinLat - minutesLat) * 60);
+
+    const degreesLon = Math.floor(lon);
+    const degToMinLon = (lon - degreesLon) * 60;
+    const minutesLon = Math.floor(degToMinLon);
+    const secondsLon = Math.floor((degToMinLon - minutesLon) * 60);
+
+  }, [city, state])
 
   return (
     <div>
