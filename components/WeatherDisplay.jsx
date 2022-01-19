f246dd4c448799b47fe46bc250dd1fbf0b33ac89
Cody Clark <opuseleven@protonmail.com>
2022-01-19 13:07:22 -0600

    Fixed

--- components/WeatherDisplay.jsx
+++ components/WeatherDisplay.jsx
@@ -1,35 +1,35 @@
 import DayDisplay from '.';
 import { useState, useEffect } from 'react';
-// import Day type & mapTo
 
-function WeatherDisplay({ weather }) {
+function WeatherDisplay({ data }) {
 
-  const [today, setToday] = useState(weather.today);
-  const [tomorrow, setTomorrow] = useState(weather.tomorrow);
-  const [dayAfter, setDayAfter] = useState(weather.dayAfter);
+  const [today, setToday] = useState(null);
+  const [tomorrow, setTomorrow] = useState(null);
+  const [dayAfter, setDayAfter] = useState(null);
 
   useEffect(() => {
-    setToday(weather.today);
-    setTomorrow(weather.tomorrow);
-    setDayAfter(weather.dayAfter);
-  }, [weather]);
+    const weatherData = data['list'];
+    setToday(weatherData[0]);
+    setTomorrow(weatherData[1]);
+    setDayAfter(weatherData[2]);
+  }, [data]);
 
   return (
     <div>
       <div>
         <h3>Today:</h3>
         <hr />
-        <DayDisplay day={today} />
-      </div>
-      <div>
-        <h3>Tomorrow:</h3>
-        <hr />
-        <DayDisplay day={tomorrow} />
-      </div>
-      <div>
-        <h3>Day After:</h3>
-        <hr />
-        <DayDisplay day={dayAfter} />
+        <div>
+          {today && (
+            <div>
+            <h3>{today['weather']['main']}</h3>
+            <p>Temperature: {today['main']['temp']}°F</p>
+            <p>Humidity: {today['main']['humidity']}%</p>
+            <p>Precipitation: {today['pop']}%</p>
+            </div>
+            )
+          }
+        </div>
       </div>
     </div>
   );
