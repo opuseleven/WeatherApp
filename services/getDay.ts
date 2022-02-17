d39da2897cf426a7bad2df7c6f90c873dfe03895
Cody Clark <opuseleven@protonmail.com>
2022-02-17 12:18:54 -0600

    Fixed flow of data to set weatherImage

--- services/getDay.ts
+++ services/getDay.ts
@@ -1,13 +1,19 @@
 import { Day } from '../types';
+import { getWeatherImage } from '../services';
 
 function getDay(dayData: any) {
+
+  const idString: any = dayData['weather'][0]['id'];
+  const image: string = getWeatherImage(idString);
+
   const day: Day = {
     weather: dayData['weather'][0]['main'],
     temp: parseInt(String((dayData['main']['temp'] * 1.8 - 459.67)), 10),
     humidity: dayData['main']['humidity'],
     weatherDescription: dayData['weather'][0]['description'],
     precipitation: Number(dayData['pop']) * 100,
-    weatherId: Number(dayData['weather'][0]['id'])
+    weatherId: Number(dayData['weather'][0]['id']),
+    weatherImage: '/../public/weather-images/' + image
   }
   return day;
 }
