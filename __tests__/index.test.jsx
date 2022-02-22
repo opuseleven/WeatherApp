399b8736e04c9e24e30f8428c9c5d725d859ea25
Cody Clark <opuseleven@protonmail.com>
2022-02-21 20:08:51 -0600

    Added tests for Home

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,24 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '../pages';
+
+describe('Home', () => {
+
+  it('Renders', () => {
+    render(<Home />);
+    const headings = screen.getAllByRole('heading');
+    expect(headings[0]).toHaveTextContent('WeatherApp');
+    expect(headings[1]).toHaveTextContent('Search by city name.');
+    expect(headings[2]).toHaveTextContent('Nashville');
+  })
+
+/*
+  it('Searches by city', async () => {
+    render(<Home />);
+    fireEvent.change(getByRole('input'), {target: {value: 'chicago'}});
+    fireEvent.click(screen.getByRole('button'));
+    const headings = screen.getAllByRole('heading');
+    await expect(headings[2]).toHaveTextContent('Chicago');
+  })
+*/
+})
