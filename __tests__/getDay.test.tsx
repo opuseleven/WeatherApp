be7e9aff9c9de00e9c27516d85e97368de5b35ea
Cody Clark <opuseleven@protonmail.com>
2022-02-21 19:53:28 -0600

    Added test for getDay function

---
+++ __tests__/getDay.test.tsx
@@ -0,0 +1,19 @@
+import { getDay } from '../services';
+import { Day } from '../types';
+
+describe('getDay function', () => {
+
+  const testdata = require('../data/testdata.json');
+  const weatherData = testdata['list'];
+
+  it('Returns a Day object', () => {
+    const day: Day = getDay(weatherData[0]);
+    expect(day.weather).toBe(weatherData[0]['weather'][0]['main']);
+    expect(day.temp).toBe(parseInt(String((weatherData[0]['main']['temp'] * 1.8 - 459.67)), 10));
+    expect(day.humidity).toBe(weatherData[0]['main']['humidity']);
+    expect(day.weatherDescription).toBe(weatherData[0]['weather'][0]['description']);
+    expect(day.precipitation).toBe(Number(weatherData[0]['pop']) * 100);
+    expect(day.weatherId).toBe(800);
+    expect(day.weatherImage).toBe('/../public/weather-images/sunny.png');
+  })
+})
