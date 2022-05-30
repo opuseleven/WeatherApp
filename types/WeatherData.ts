1b7cc18f2448f4f1ce3dd44b1b896ed12d761179
Cody Clark <opuseleven@protonmail.com>
2022-05-29 23:38:29 -0500

    Added type WeatherData

---
+++ types/WeatherData.ts
@@ -0,0 +1,35 @@
+export interface WeatherData {
+  dt: number,
+  main: {
+    temp: number,
+    feels_like: number,
+    temp_min: number,
+    temp_max: number,
+    pressure: number,
+    sea_level: number,
+    grnd_level: number,
+    humidity: number,
+    temp_kf: number
+  },
+  weather: [
+    {
+      id: number,
+      main: string,
+      description: string,
+      icon: string
+    }
+  ],
+  clouds: {
+    all: number
+  },
+  wind: {
+    speed: number,
+    deg: number
+  },
+  visibility: number,
+  pop: number,
+  sys: {
+    pod: string
+  },
+  dt_txt: string
+}
