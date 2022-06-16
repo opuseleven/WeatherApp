09eed2fb3fcff33953a1996fd3204f30cbb34bb1
Cody Clark <opuseleven@protonmail.com>
2022-06-16 14:24:45 -0500

    Fixed link & converted to typescript

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <h5>Data provided by: </h5>
+      <h6><a href="https://www.openweathermap.org">openweathermap.org</a></h6>
+    </div>
+  );
+}
+export { Footer };
