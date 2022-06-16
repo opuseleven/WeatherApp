167867c765138808b5c4510aa369ce78dabe92a4
Cody Clark <opuseleven@protonmail.com>
2022-06-16 14:24:29 -0500

    Changed Banner component to typescript

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,11 @@
-function Banner() {
+const Banner: React.FC = () => {
+
   return (
     <div>
       <header>
         <h1>WeatherApp</h1>
       </header>
     </div>
-  );
+  )
 }
 export { Banner };
