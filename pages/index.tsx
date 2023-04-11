93855967f54f850960f019e83adc0c5621e60d62
Cody Clark <opuseleven@protonmail.com>
2023-04-11 00:25:11 -0500

    Added a City state variable, and another useEffect hook to handle geocoding before getting weather data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,81 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Banner, Info, Footer, WeatherDisplay, CityDisplay } from '../components'
-import { useField } from '../hooks';
+import { Banner, Info, SearchForm, CityDisplay, WeatherDisplay, Footer } from '../components'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { ApiData } from '../types';
+import { ApiData, City, ZipData, CityData } from '../types';
+import { cityError, forecastError } from '../errors';
 
 const Home: NextPage = () => {
 
-  const [citySearch, setCitySearch] = useState<string>("Nashville");
+  const [citySearch, setCitySearch] = useState<string>("Nashville, TN");
+  const [zipSearch, setZipSearch] = useState<boolean>(false);
+  const [city, setCity] = useState<City | null>();
   const tempData: ApiData = require('../data/testdata.json');
   const [data, setData] = useState<ApiData>(tempData);
   const apiKey = process.env.API_KEY;
-  const searchInput = useField('text');
+  const testUrl = '/api/testdata' // only used for testing
 
-  function refreshData() {
-    const newUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + '&cnt=3&appid=' + apiKey;
-    const testUrl = '/api/testdata' // only used for testing
-    axios
-      .request({url: newUrl}).then((response) => setData(response.data));
-  }
 
-  function handleClick(e: any) {
-    e.preventDefault();
-    setCitySearch(searchInput.value);
-    refreshData();
+  useEffect(() => {
+    if (citySearch !== '') {
+      const coordsUrl = 'http://api.openweathermap.org/geo/1.0/'
+                        + (zipSearch ? 'zip?zip=' : 'direct?q=') + citySearch
+                        + (zipSearch ? '' : 'limit=3') + '&appid=' + apiKey;
+      axios
+        .request({
+          url: coordsUrl,
+          method: 'get',
+          responseType: 'json',
+        })
+        .then((res) => handleCityResults(res.data))
+        .catch((err) => {
+          console.log(err);
+          const error = cityError();
+          setData(error);
+        })
+    }
+  }, [citySearch]);
+
+  function handleCityResults(data: any) {
+    let returnCity: City | null;
+    if (zipSearch) {
+      const zipData: ZipData = data;
+      returnCity = {
+        name: zipData.name,
+        lat: String(zipData.lat),
+        lon: String(zipData.lon)
+      }
+    } else {
+      const cityData: CityData = data[0];
+      returnCity = {
+        name: cityData.name,
+        lat: String(cityData.lat),
+        lon: String(cityData.lon)
+      }
+    }
+    setCity(returnCity);
   }
 
   useEffect(() => {
-    refreshData()
-  }, []);
+    if (city) {
+      const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat='
+                  + city.lat + '&lon=' + city.lon + '&cnt=3&appid=' + apiKey;
+      axios
+        .request({
+          url: forecastUrl,
+          method: 'get',
+          responseType: 'json',
+        })
+        .then((res) => setData(res.data))
+        .catch((err) => {
+          console.log(err);
+          const error = forecastError();
+          setData(error);
+        })
+    }
+  }, [city])
 
   return (
     <div className={styles.container}>
@@ -50,10 +95,9 @@ const Home: NextPage = () => {
         </div>
 
         <div>
-          <form onSubmit={handleClick}>
-            <input {...searchInput} />
-            <button>Search</button>
-          </form>
+          <SearchForm setCitySearch={setCitySearch} zip={zipSearch}
+            setZip={setZipSearch}
+          />
         </div>
 
         <div>
