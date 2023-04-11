c2d3455bba6e1f93cf98986e3770c17ad316d145
Cody Clark <opuseleven@protonmail.com>
2023-04-11 14:05:37 -0500

    Expanded Info component to help with formatting and provide info on new zip code search

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,6 +3,10 @@ const Info: React.FC = () => {
   return(
     <div>
       <h2>Search by city name.</h2>
+      <ul>
+        <li><h4>Format: "Nashville, TN"</h4></li>
+        <li><h4>Or search by zip code with the dropdown!</h4></li>
+      </ul>
     </div>
   );
 }
