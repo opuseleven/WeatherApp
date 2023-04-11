044c571cd57fbb02e1b593e329b8e8a2bc99d6cf
Cody Clark <opuseleven@protonmail.com>
2023-04-11 15:02:53 -0500

    Fixed string to use escape keys for quotes

--- components/Info.tsx
+++ components/Info.tsx
@@ -4,7 +4,7 @@ const Info: React.FC = () => {
     <div>
       <h2>Search by city name.</h2>
       <ul>
-        <li><h4>Format: "Nashville, TN"</h4></li>
+        <li><h4>Format: &#34;Nashville, TN&#34;</h4></li>
         <li><h4>Or search by zip code with the dropdown!</h4></li>
       </ul>
     </div>
