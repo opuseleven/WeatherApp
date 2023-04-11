3f55021c4b5eb7548769cd7e6dcaaed44364d496
Cody Clark <opuseleven@protonmail.com>
2023-04-11 14:24:00 -0500

    Fixed the way coordinates are displayed in CityDisplay component

--- components/CityDisplay.tsx
+++ components/CityDisplay.tsx
@@ -9,11 +9,29 @@ interface CityDisplayProps {
 const CityDisplay: React.FC<CityDisplayProps> = ({ data }) => {
 
   const [city, setCity] = useState<City | undefined>();
+  const [displayedCoords, setDisplayedCoords] = useState<string>('');
 
   useEffect(() => {
     const newCity = getCity(data['city']);
     setCity(newCity);
+    setDisplayedCoords(getDisplayedCoords(newCity));
   }, [data])
+
+  function getDisplayedCoords(city: City) {
+    let coords = city.lat + '°';
+    if (Number(city.lat) > 0) {
+      coords = coords + 'N ';
+    } else {
+      coords = coords + 'S ';
+    }
+    coords = coords + 'x ' + city.lon + '°';
+    if (Number(city.lon) > 0) {
+      coords = coords + 'E';
+    } else {
+      coords = coords + 'W';
+    }
+    return coords;
+  }
 
   return (
     <div>
@@ -21,7 +39,7 @@ const CityDisplay: React.FC<CityDisplayProps> = ({ data }) => {
         city && (
           <div>
             <h2>{city.name}</h2>
-            <p>{data['city']['coord']['lat']}° {data['city']['coord']['lon']}</p>
+            <p>{displayedCoords}</p>
           </div>
         )
       }
