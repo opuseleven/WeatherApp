bed5228054692c3da1ed76e3f3cc60423e9dce68
Cody Clark <opuseleven@protonmail.com>
2023-04-18 21:56:43 -0500

    Added additional tests

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../pages';
 
@@ -9,17 +9,47 @@ describe('Home', () => {
     const headings = screen.getAllByRole('heading');
     expect(headings[0]).toHaveTextContent('WeatherApp');
     expect(headings[1]).toHaveTextContent('Search by city name.');
-    expect(headings[2]).toHaveTextContent('Nashville');
+    expect(headings[2]).toHaveTextContent('Format: "Nashville, TN"');
+    expect(headings[3]).toHaveTextContent('Or search by zip code');
+    expect(headings[4]).toHaveTextContent('Nashville');
   })
 
-/*
   it('Searches by city', async () => {
     render(<Home />);
-    fireEvent.change(getByRole('input'), {target: {value: 'chicago'}});
-    fireEvent.click(screen.getByRole('button'));
-    await screen.findByText('Chicago');
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 1000))
+    })
+    act(() => {
+      fireEvent.change(screen.getByRole('textbox'), {target: {value: 'chicago, il'}});
+    })
+    act(() => {
+      fireEvent.click(screen.getByRole('button'));
+    })
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 2000));
+    })
     const headings = screen.getAllByRole('heading');
-    expect(headings[2]).toHaveTextContent('Chicago');
+    expect(headings[4]).toHaveTextContent('Chicago');
   })
-*/
+
+  it('Searches by zip code', async () => {
+    render(<Home />);
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 1000))
+    })
+    act(() => {
+      fireEvent.change(screen.getByRole('combobox'), {target: {value: 'Zip Code Search'}});
+    })
+    act(() => {
+      fireEvent.change(screen.getByRole('textbox'), {target: {value: '97862'}});
+    })
+    act(() => {
+      fireEvent.click(screen.getByRole('button'));
+    })
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 2000));
+    })
+    const headings = screen.getAllByRole('heading');
+    expect(headings[4]).toHaveTextContent('Milton-Freewater');
+  })
 })
