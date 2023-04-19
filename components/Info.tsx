fb696722330b48383ccfb24fb7a69fd8cd86a0c8
Cody Clark <opuseleven@protonmail.com>
2023-04-19 12:31:52 -0500

    Fixed info now that formatting does not need to be specific

--- components/Info.tsx
+++ components/Info.tsx
@@ -3,10 +3,7 @@ const Info: React.FC = () => {
   return(
     <div>
       <h2>Search by city name.</h2>
-      <ul>
-        <li><h4>Format: &#34;Nashville, TN&#34;</h4></li>
-        <li><h4>Or search by zip code with the dropdown!</h4></li>
-      </ul>
+      <h3>Or search by zip code with the dropdown!</h3>
     </div>
   );
 }
