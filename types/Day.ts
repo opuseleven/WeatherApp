66a912ca34608c95713bc71d12f4d5e1e0eafbd4
Cody Clark <opuseleven@protonmail.com>
2023-04-24 18:29:40 -0500

    Added feels_like, low_temp, high_temp, cloud_coverage, wind_speed, gusts, & visibility params

--- types/Day.ts
+++ types/Day.ts
@@ -1,9 +1,16 @@
 export interface Day {
   weather: string,
   temp: number,
+  feels_like: number,
+  low_temp: number,
+  high_temp: number,
   humidity: string,
   weatherDescription: string,
   precipitation: number,
+  cloud_coverage: number,
+  wind_speed: number,
+  gusts: number,
+  visibility: number,
   weatherId: number,
   weatherImage: string
 }
