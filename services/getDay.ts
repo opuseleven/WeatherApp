67dc41bdfb421466aa5fdfd0e2729843601b5b36
Cody Clark <opuseleven@protonmail.com>
2023-04-24 18:30:47 -0500

    Fixed getDay function to parse new Day params

--- services/getDay.ts
+++ services/getDay.ts
@@ -9,9 +9,16 @@ function getDay(dayData: any) {
   const day: Day = {
     weather: dayData['weather'][0]['main'],
     temp: parseInt(String((dayData['main']['temp'] * 1.8 - 459.67)), 10),
+    feels_like: parseInt(String((dayData['main']['feels_like'] * 1.8 - 459.67)), 10),
+    low_temp: parseInt(String((dayData['main']['temp_min'] * 1.8 - 459.67)), 10),
+    high_temp: parseInt(String((dayData['main']['temp_max'] * 1.8 - 459.67)), 10),
     humidity: dayData['main']['humidity'],
     weatherDescription: dayData['weather'][0]['description'],
     precipitation: Number(dayData['pop']) * 100,
+    cloud_coverage: dayData['clouds']['all'],
+    wind_speed: dayData['wind']['speed'],
+    gusts: dayData['wind']['gust'],
+    visibility: dayData['visibility'],
     weatherId: Number(dayData['weather'][0]['id']),
     weatherImage: `/weather-images/` + image
   }
