53a46f2f14523a26d2794df1f2e5ecab99084122
Cody Clark <opuseleven@protonmail.com>
2023-04-25 18:41:37 -0500

    Fixed RenderImage component & changed to typescript

--- __tests__/RenderImage.test.tsx
+++ __tests__/RenderImage.test.tsx
@@ -5,8 +5,8 @@ import { RenderImage } from '../components';
 describe('RenderImage', () => {
 
   it('Renders component with image', async () => {
-    render(<RenderImage image={'/../public/weather-images/cloudy.png'} />);
-    await(screen.getByRole('img'));
+    render(<RenderImage image={'/../public/weather-images/cloudy.png'} alt="alt text" />);
+    await (screen.getByRole('img'));
     expect(screen.getByRole('img')).toBeDefined();
   })
 })
