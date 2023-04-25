546e12520cf45fc4786cb29babfaae771abd6bbe
Cody Clark <opuseleven@protonmail.com>
2023-04-25 09:43:07 -0500

    Expanded CityDisplay component to convert and display sunrise and sunset times.

--- components/CityDisplay.tsx
+++ components/CityDisplay.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getCity } from '../services';
 import { ApiData, City } from '../types';
+import styles from '../styles/Components.module.css';
 
 interface CityDisplayProps {
   data: ApiData
@@ -10,11 +11,16 @@ const CityDisplay: React.FC<CityDisplayProps> = ({ data }) => {
 
   const [city, setCity] = useState<City | undefined>();
   const [displayedCoords, setDisplayedCoords] = useState<string>('');
+  const [sunrise, setSunrise] = useState<string>('');
+  const [sunset, setSunset] = useState<string>('');
 
   useEffect(() => {
     const newCity = getCity(data['city']);
     setCity(newCity);
     setDisplayedCoords(getDisplayedCoords(newCity));
+    const sunriseSunset = getSunriseSunset(data);
+    setSunrise(convertTo12HourTime(sunriseSunset[0]));
+    setSunset(convertTo12HourTime(sunriseSunset[1]));
   }, [data])
 
   function getDisplayedCoords(city: City) {
@@ -33,13 +39,44 @@ const CityDisplay: React.FC<CityDisplayProps> = ({ data }) => {
     return coords;
   }
 
+  function getSunriseSunset(data: ApiData) {
+    let rise = '';
+    let set = '';
+    function getTime(d: Date) {
+      const hours = d.getHours();
+      let minutes = String(d.getMinutes());
+      if (minutes.length != 2) {
+        minutes = '0' + minutes;
+      }
+      return hours + ':' + minutes;
+    }
+    const sunriseDate = new Date(data['city']['sunrise'] * 1000);
+    const sunsetDate = new Date(data['city']['sunset'] * 1000);
+    rise = getTime(sunriseDate);
+    set = getTime(sunsetDate);
+    return [rise, set];
+  }
+
+  function convertTo12HourTime(time: string) {
+    let returnTime = '';
+    let [hours, minutes] = time.split(':');
+    let period = 'AM';
+    if (Number(hours) > 11) {
+      period = 'PM';
+      hours = String(Number(hours) - 12);
+    }
+    returnTime = hours + ':' + minutes + period;
+    return returnTime;
+  }
+
   return (
     <div>
       {
         city && (
-          <div>
+          <div className={styles.citydisplaycontainer}>
             <h2>{city.name}</h2>
             <h4>{displayedCoords}</h4>
+            <h5>Sunrise: {sunrise} <hr />  Sunset: {sunset}</h5>
           </div>
         )
       }
