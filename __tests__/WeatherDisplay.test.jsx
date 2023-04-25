0abbe42174557097080a433acb5702c22d637519
Cody Clark <opuseleven@protonmail.com>
2023-04-25 16:02:34 -0500

    Added additional tests for WeatherDisplay component

--- __tests__/WeatherDisplay.test.jsx
+++ __tests__/WeatherDisplay.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { WeatherDisplay } from '../components';
 
@@ -9,8 +9,49 @@ describe('WeatherDisplay Component', () => {
   it('Renders and passes data', () => {
     render(<WeatherDisplay data={testdata} />);
     const headings = screen.getAllByRole('heading');
-    expect(headings[1]).toHaveTextContent('Clear');
-    expect(headings[3]).toHaveTextContent('Clear');
+    const details = screen.getAllByRole('detail');
+    expect(headings[1]).toHaveTextContent('Today:');
+    expect(headings[2]).toHaveTextContent('Clear');
+    expect(headings[4]).toHaveTextContent('Tomorrow:');
     expect(headings[5]).toHaveTextContent('Clear');
+    expect(headings[7]).toHaveTextContent('Day After:');
+    expect(headings[8]).toHaveTextContent('Clear');
+    expect(details[0]).toHaveTextContent('clear sky');
+    expect(details[1]).toHaveTextContent('Temperature: 61°F');
+    expect(details[2]).toHaveTextContent('Humidity: 31%');
+    expect(details[3]).toHaveTextContent('Precipitation: 0%');
+  })
+
+  it('Today expands onClick', () => {
+    render(<WeatherDisplay data={testdata} />);
+    let headings = screen.getAllByRole('heading');
+    expect(headings[0]).toHaveTextContent(String.fromCharCode(9656));
+    act(() => {
+      fireEvent.click(screen.getByText('Today:'));
+    })
+    headings = screen.getAllByRole('heading');
+    expect(headings[0]).toHaveTextContent(String.fromCharCode(9662));
+  })
+
+  it('Tomorrow expands onClick', () => {
+    render(<WeatherDisplay data={testdata} />);
+    let headings = screen.getAllByRole('heading');
+    expect(headings[3]).toHaveTextContent(String.fromCharCode(9656));
+    act(() => {
+      fireEvent.click(screen.getByText('Tomorrow:'));
+    })
+    headings = screen.getAllByRole('heading');
+    expect(headings[3]).toHaveTextContent(String.fromCharCode(9662));
+  })
+
+  it('DayAfter expands onClick', () => {
+    render(<WeatherDisplay data={testdata} />);
+    let headings = screen.getAllByRole('heading');
+    expect(headings[6]).toHaveTextContent(String.fromCharCode(9656));
+    act(() => {
+      fireEvent.click(screen.getByText('Day After:'));
+    })
+    headings = screen.getAllByRole('heading');
+    expect(headings[6]).toHaveTextContent(String.fromCharCode(9662));
   })
 })
