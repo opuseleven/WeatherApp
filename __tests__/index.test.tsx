5e047baf44f6e0a02ca8fb995b0473851518d545
Cody Clark <opuseleven@protonmail.com>
2023-04-27 18:44:21 -0500

    Added screen.debug()

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -102,6 +102,7 @@ describe('Home', () => {
       await new Promise(r => setTimeout(r, 1000));
     })
     expect(await screen.findByText('Error: Error finding that city.')).toBeInTheDocument();
+    screen.debug();
   })
 
   it('Returns an error if zip search fails', async () => {
